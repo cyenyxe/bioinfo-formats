932e86726b3bacd4e863655789b33a347c5e6a42
imedina <imedina@cipf.es>
2011-09-14 20:01:38 +0200

    Uniprot: parser for v1.40 has been addded

--- src/main/java/org/bioinfo/formats/parser/uniprot/UniprotParser.java
+++ src/main/java/org/bioinfo/formats/parser/uniprot/UniprotParser.java
@@ -12,11 +12,12 @@ import javax.xml.bind.Unmarshaller;
 
 public class UniprotParser {
 
-	private final static String UNIPROT_CONTEXT = "org.bioinfo.formats.parser.uniprot.v135jaxb";
+	public final static String UNIPROT_CONTEXT_v135 = "org.bioinfo.formats.parser.uniprot.v135jaxb";
+	public final static String UNIPROT_CONTEXT_v140 = "org.bioinfo.formats.parser.uniprot.v140jaxb";
 
 	public static void saveXMLInfo(Object obj, String filename) throws FileNotFoundException, JAXBException {
 		JAXBContext jaxbContext;
-		jaxbContext = JAXBContext.newInstance(UNIPROT_CONTEXT);
+		jaxbContext = JAXBContext.newInstance(UNIPROT_CONTEXT_v135);
 		Marshaller marshaller = jaxbContext.createMarshaller();
 		marshaller.marshal(obj, new FileOutputStream(filename));	
 	}
@@ -28,7 +29,20 @@ public class UniprotParser {
 	 */
 	public static Object loadXMLInfo(String filename) throws JAXBException {
 		Object obj = null;
-		JAXBContext jaxbContext = JAXBContext.newInstance(UNIPROT_CONTEXT);
+		JAXBContext jaxbContext = JAXBContext.newInstance(UNIPROT_CONTEXT_v140);
+		Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
+		obj =  unmarshaller.unmarshal(new File(filename));
+		return obj;
+	}
+	
+	/**
+	 * Checks if XML info file exists and loads it
+	 * @throws JAXBException 
+	 * @throws IOException
+	 */
+	public static Object loadXMLInfo(String filename, String uniprotVersion) throws JAXBException {
+		Object obj = null;
+		JAXBContext jaxbContext = JAXBContext.newInstance(uniprotVersion);
 		Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
 		obj =  unmarshaller.unmarshal(new File(filename));
 		return obj;
