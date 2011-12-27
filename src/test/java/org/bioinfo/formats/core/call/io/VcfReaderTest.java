c29cfff61f260b566aa2bd4879b9c54805d445b0
imedina <imedina@cipf.es>
2011-12-27 15:46:20 +0100

    VCF4 parser done

--- src/test/java/org/bioinfo/formats/core/call/io/VcfReaderTest.java
+++ src/test/java/org/bioinfo/formats/core/call/io/VcfReaderTest.java
@@ -2,8 +2,11 @@ package org.bioinfo.formats.core.call.io;
 
 import static org.junit.Assert.fail;
 
-import org.bioinfo.formats.core.call.Vcf4;
-import org.bioinfo.formats.core.call.vcf4.VcfRecord;
+import org.bioinfo.commons.io.utils.IOUtils;
+import org.bioinfo.formats.core.variant.Vcf4;
+import org.bioinfo.formats.core.variant.io.Vcf4Reader;
+import org.bioinfo.formats.core.variant.vcf4.filter.VcfInfoFilter;
+import org.bioinfo.formats.core.variant.vcf4.filter.VcfRegionFilter;
 import org.junit.Test;
 
 public class VcfReaderTest {
@@ -13,12 +16,24 @@ public class VcfReaderTest {
 		System.out.println("\nTest 1 - read()");
 
 		try {
-			Vcf4Reader vcf4Reader = new Vcf4Reader("/mnt/commons/test/formats/features/example_1000genomes_40000.vcf");
+			Vcf4Reader vcf4Reader = new Vcf4Reader("/opt/variant/example/01-12156-ASW.vcf");
 			
-			Vcf4 vcf4 = vcf4Reader.parse(); 
+//			List<VcfGenericFilter> filters = new ArrayList<VcfGenericFilter>();
+//			filters.add(new VcfRegionFilter("1", 50, 100000));
+//			vcf4Reader.setVcfGenericFilters(filters);
+			vcf4Reader.addFilter(new VcfRegionFilter("1", 50, 2000000));
+			vcf4Reader.addFilter(new VcfRegionFilter("1", 18000000, 20000000));
+			vcf4Reader.addFilter(new VcfInfoFilter("AF<0.05"));
 			
-			System.out.println(vcf4.getRecords().toString());
+			Vcf4 vcf4 = vcf4Reader.parse();
 			
+			
+//			System.out.println(vcf4.getInfo().toString());
+//			for(VcfRecord vcfRec: vcf4.getRecords()) {
+//				System.out.println(vcfRec.toString());
+//				
+//			}
+			IOUtils.write("/tmp/aaa.txt", vcf4.toString());
 //			VcfRecord vcfRecord = null;
 //			while((vcfRecord = vcf4Reader.read()) != null) {
 ////				System.out.println(vcfRecord.toString());
